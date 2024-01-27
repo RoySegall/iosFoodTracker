1df16d7ec70ce5d8811d1b13807aa70a5d7aee27
Roy Segall <roy@segall.io>
2024-01-27 08:27:39 +0200

    Commit for now

--- FoodTracker/FoodItemsList.swift
+++ FoodTracker/FoodItemsList.swift
@@ -2,40 +2,37 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var foodItemsStore = FoodItemsStore()
-    @State private var isSheetPresented = false
     
     var body: some View {
-        
         NavigationView {
             List {
                 Section(header: Text("Fruits")) {
                     ForEach(self.foodItemsStore.foodItems) { item in
-                            HStack {
-                                Text(item.emoji).font(.largeTitle)
-                                
-                                VStack(alignment: .leading) {
-                                    Text(item.name)
-                                        .font(.headline)
-                                        .textCase(.uppercase)
-                                    Text(item.location).font(.subheadline)
+                        HStack {
+                            Text(item.emoji).font(.custom("emoji", fixedSize: 50.0))
+                            
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text(item.name).font(.headline).textCase(.uppercase)
+                                    Spacer()
+                                    ForEach(0 ..< 5) { index in
+                                        Text("⭐️").font(.custom("rating", fixedSize: 15.0))
+                                    }
                                 }
-                                Spacer()
-                                Text("⭐️")
+                                
+                                Text(item.location).font(.subheadline).padding(.top, 1)
                             }
-                        
+                            Spacer()
+                        }
                     }
                     HStack {
                         Spacer()
-                        Button(action: {
-                        }) {
-                            Text("➕ Add more items")
-                                .font(.title3)
-                                .foregroundStyle(Color.blue)
+                        Button(action: {}) {
+                            Text("➕ Add more items").font(.title3).foregroundStyle(Color.blue)
                         } 
                         .buttonStyle(PlainButtonStyle())
                         Spacer()
                     }.padding()
-                    
                 }
             }
         }
